93a37ef9bcc4ac2507c23508c62ddf08c3eff083
Kateryna Vasiuk <catherine.vasyuk11@gmail.com>
2023-09-15 02:30:15 +0300

    unique solution

--- src/components/UploadField/UploadField.jsx
+++ src/components/UploadField/UploadField.jsx
@@ -1,109 +1,125 @@
 import { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import { setData } from 'store/studentsSlice';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
+import FormatSizeIcon from '@mui/icons-material/FormatSize';
+import { setStudents } from 'store/studentsSlice';
 import { useDispatch } from 'react-redux';
-import { StyledWrap, VisuallyHiddenInput } from './UploadField.styled';
+import { ListStyled, VisuallyHiddenInput } from './UploadField.styled';
 import { toast } from 'react-toastify';
-import { matchStudentsWithTeachers } from './matchStudentsWithTeachers';
 
 const UploadField = () => {
-  const [StudentFileData, setStudentFileData] = useState('');
-  const [TeacherFileData, setTeacherFileData] = useState('');
-  const [students, setStudents] = useState([]);
-  const [teachers, setTeachers] = useState([]);
+  const [uploadedFile, setUploadedFile] = useState(null);
+  const [fileData, setFileData] = useState('');
   const dispatch = useDispatch();
-
   const handleFileUpload = e => {
     if (e.target.files[0].type !== 'text/plain') {
       toast.error('You upload wrong type document!');
       return;
     }
-    if (e.target.name === 'student') {
-      const file = e.target.files[0];
-      const reader = new FileReader();
-      reader.onload = event => {
-        const content = event.target.result;
-        setStudentFileData(content);
-      };
-      reader.readAsText(file);
-    } else {
-      const file = e.target.files[0];
-      const reader = new FileReader();
-      reader.onload = event => {
-        const content = event.target.result;
-        setTeacherFileData(content);
-      };
-      reader.readAsText(file);
-    }
+
+    const file = e.target.files[0];
+    setUploadedFile(file);
+    const reader = new FileReader();
+    reader.onload = event => {
+      const content = event.target.result;
+      setFileData(content);
+    };
+    reader.readAsText(file);
   };
 
   useEffect(() => {
-    const students = StudentFileData.trim()
+    const students = fileData
+      .trim()
       .split('\n')
       .map(line => {
-        const [lastName, firstName, grade, classroom, bus] = line
-          .trim()
-          .split(',')
-          .map(item => item.trim());
+        const [
+          lastName,
+          firstName,
+          grade,
+          classroom,
+          bus,
+          teacherLastName,
+          teacherName,
+        ] = line.trim().split(',');
         return {
           lastName,
           firstName,
           grade,
           classroom,
           bus,
-        };
-      });
-    const teachers = TeacherFileData.trim()
-      .split('\n')
-      .map(line => {
-        const [teacherLastName, teacherName, classroom] = line
-          .trim()
-          .split(',')
-          .map(item => item.trim());
-        return {
           teacherLastName,
           teacherName,
-          classroom,
         };
       });
-    setTeachers(teachers);
-    setStudents(students);
-  }, [StudentFileData, TeacherFileData]);
 
-  useEffect(() => {
-    if (students.length > 1 && teachers.length > 1) {
-      const studentsWithTeachers = matchStudentsWithTeachers(
-        students,
-        teachers
-      );
-      dispatch(setData(studentsWithTeachers));
-    }
-  }, [students, teachers, dispatch]);
+    const uniqueStudents = students.filter(
+      (student, index, self) =>
+        index ===
+        self.findIndex(
+          s =>
+            s.grade === student.grade &&
+            s.lastName === student.lastName &&
+            s.firstName === student.firstName &&
+            s.classroom === student.classroom &&
+            s.bus === student.bus &&
+            s.teacherLastName === student.teacherLastName &&
+            s.teacherName === student.teacherName
+        )
+    );
+    dispatch(setStudents(uniqueStudents));
+  }, [fileData, dispatch]);
   return (
     <>
-      <StyledWrap>
-        <Button
-          component="label"
-          variant="contained"
-          startIcon={<CloudUploadIcon />}
-          href="#file-upload"
-          onChange={handleFileUpload}
+      <Button
+        component="label"
+        variant="contained"
+        startIcon={<CloudUploadIcon />}
+        href="#file-upload"
+        onChange={handleFileUpload}
+      >
+        Upload a file
+        <VisuallyHiddenInput type="file" />
+      </Button>
+      {uploadedFile && (
+        <ListStyled
+          primary="Name"
+          sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
         >
-          Upload a student file
-          <VisuallyHiddenInput type="file" name="student" />
-        </Button>
-        <Button
-          component="label"
-          variant="contained"
-          startIcon={<CloudUploadIcon />}
-          href="#file-upload"
-          onChange={handleFileUpload}
-        >
-          Upload a teacher file
-          <VisuallyHiddenInput type="file" name="teacher" />
-        </Button>
-      </StyledWrap>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <AccountCircleIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Name" secondary={uploadedFile.name} />
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <FormatSizeIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Type" secondary={uploadedFile.type} />
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <FullscreenExitIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary="Size"
+              secondary={`${uploadedFile.size} bytes`}
+            />
+          </ListItem>
+        </ListStyled>
+      )}
     </>
   );
 };
